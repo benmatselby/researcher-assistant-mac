29d51b86129f1834d4130b9801d0aa44a38fc114
Ben Selby <ben.selby@prolific.co>
2022-12-23 14:22:15 +0000

    Use a Keychain library rather than maintaining our own
    This is easier to work with in the code. This also means that *the token
    is sycned to iCloud* which I think helps us with other apps we may want
    to build.

--- ProlificAssistant/Client/ApiConfig.swift
+++ ProlificAssistant/Client/ApiConfig.swift
@@ -6,15 +6,27 @@
 //
 
 import Foundation
+import KeychainAccess
 
-/// Responsible for understanding configuration for talking to the Prolific API
+/// Responsible for understanding configuration for talking to the Prolific API.
 class ApiConfig {
-  /// API URL
+  /// API URL to connect to Prolific.
   var apiURL: String
+
+  /// The keychain object we are using to store/retrieve the API Token.
+  var keychain: Keychain
+
+  /// The Key we use in the Keychain item.
+  var keychainKey: String = "apiToken"
 
   /// Constructor
   init() {
     self.apiURL = "https://api.prolific.co/api"
+
+    let bundle = Bundle.main.bundleIdentifier!
+    self.keychain = Keychain(service: bundle)
+      .label("api.prolific.co")
+      .synchronizable(true)
   }
 
   /// Check to see if the API Config is valid
@@ -31,21 +43,19 @@ class ApiConfig {
   ///
   /// - Parameter apiToken: The Token used to talk to Prolific
   func setApiToken(apiToken: String) {
-    Keychain.setApiToken(
-      url: self.getApiUrl(),
-      password: apiToken,
-      identifier: Bundle.main.bundleIdentifier ?? ""
-    )
+    self.keychain[self.keychainKey] = apiToken
   }
 
   /// Retrieve the API Token
   ///
   /// - Returns: API Token from Keychina
   func getApiToken() -> String {
-    return Keychain.getApiToken(
-      url: self.getApiUrl(), identifier: Bundle.main.bundleIdentifier ?? "")
+    return self.keychain[self.keychainKey] ?? ""
   }
 
+  /// Get the API URL
+  ///
+  /// - Returns: An API URL
   func getApiUrl() -> String {
     return self.apiURL
   }
